350a3912e8a67aa8b6fc2ea1afc4515207422198
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-19 17:37:39 -0400

    Added data models for request types and API keys

---
+++ src/data/models.go
@@ -0,0 +1,21 @@
+package data
+
+// APIKey is the key used to authenticate requests
+type APIKey string
+
+//SMS is the basic struct for messages
+type SMS struct {
+	Message string `json:"message"`
+	Target  string `json:"target"`
+}
+
+//CastSMS is the basic struct for sending a single message with multiple targets
+type CastSMS struct {
+	Targets []string `json:"targets"`
+	Message string   `json:"message"`
+}
+
+//BatchSMS is the holder for many messages with many targets
+type BatchSMS struct {
+	Messages []SMS `json:"messages"`
+}
