87c2c35b3c89866ead8c26297d2ce7381c110c36
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-19 17:38:25 -0400

    Tightened up config.go to reflect config.json structure

---
+++ src/util/config.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	// Conf Global configuration
+	Conf Configuration
+	// Templates stores statically compiled HTML templates
+	Templates = template.Must(template.ParseFiles("web/index.html"))
+)
+
+// Configuration for csms-backend
+type Configuration struct {
+	Port    string `json:"port"`
+	Prefix  string `json:"prefix"`
+	Rate    int    `json:"rate"`
+	Version string `json:"version"`
+}
+
+// ReadConfig reads config.json into memory for global configuration
+func ReadConfig() Configuration {
+	raw, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal("Configuration Error! config.json improperly loaded or does not exist :: " + err.Error())
+		os.Exit(1)
+	}
+
+	var c Configuration
+	if errUnm := json.Unmarshal(raw, &c); errUnm != nil {
+		log.Fatal("Configuration Error! config.json improperly unmarshaled :: " + errUnm.Error())
+		os.Exit(1)
+	}
+
+	c.Prefix = c.Prefix + "/v" + c.Version
+
+	return c
+}
