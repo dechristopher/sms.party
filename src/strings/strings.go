c47802bdb7a47ba69b510af9d17f68fb97ca7b31
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-19 17:40:47 -0400

    Added startup string to strings.go

--- src/strings/strings.go
+++ src/strings/strings.go
@@ -1,18 +1,16 @@
 package strings
 
 const (
+	// InfoStartup notifies of service startup
+	InfoStartup = "~ Init sms.party backend v"
 	// InfoDefaultPort notifies of default port usage
 	InfoDefaultPort = "~ Defaulting port bind to port 80 because no $PORT env var proviced at runtime."
 	// ErrBadAPIKey error message for bad API key response
-	ErrBadAPIKey = "Invalid cSMS API key"
+	ErrBadAPIKey = "Invalid sms.party API key"
 	// ErrMessageTooLong error message for SMS > 160 characters
 	ErrMessageTooLong = "Message too long, max is 160 characters"
-	// ErrInsufficientCredit error message for not enough credit on account
-	ErrInsufficientCredit = "Insufficient credit on account"
 	// ErrInternalServerError error for internal server logic error
-	ErrInternalServerError = "A problem has occured within cSMS, please try again momentarily"
+	ErrInternalServerError = "A problem has occured within sms.party, please try again momentarily"
 	// ErrBadRequest error for invalid request to API
 	ErrBadRequest = "Invalid request format, check your formatting"
-	// ErrBadPromoCode error for invalid promo code or expired/already used
-	ErrBadPromoCode = "Promo code invalid, expired, or already used"
 )
