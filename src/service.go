ae74fabfc9a6ccd3e936706b1cbbc5d63366c37a
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-20 10:12:11 -0400

    Fleshed out entire API and changed most handlers to UnimplementedHandler

--- src/service.go
+++ src/service.go
@@ -35,29 +35,36 @@ func main() {
 	// Instantiate new bone mux
 	mux := bone.New()
 
-	// API Endpoints
+	/*--- START API ENDPOINTS ---*/
 	// POST /sms/batch sends a group of messages all sent to the API at once to conserve bandwidth
-	// mux.Post(u.Conf.Prefix+"/sms/batch", http.HandlerFunc(BatchHandler))
-
+	mux.Post(u.Conf.Prefix+"/sms/batch", http.HandlerFunc(a.UnimplementedHandler))
 	// POST /sms/cast sends a single SMS message to multiple numbers
-	// mux.Post(u.Conf.Prefix+"/sms/cast", http.HandlerFunc(CastHandler))
-
+	mux.Post(u.Conf.Prefix+"/sms/cast", http.HandlerFunc(a.UnimplementedHandler))
 	// POST /sms/send sends a single SMS message to a single number
 	mux.Post(u.Conf.Prefix+"/sms/send", http.HandlerFunc(a.SendHandler))
-
 	// GET /sms returns information about total app sms statistics
-	mux.Get(u.Conf.Prefix+"/sms", http.HandlerFunc(a.SendHandler))
+	mux.Get(u.Conf.Prefix+"/sms", http.HandlerFunc(a.UnimplementedHandler))
+	/*--- END API ENDPOINTS ---*/
 
-	// Helper Endpoints
+	/*--- START HELPER ENDPOINTS ---*/
 	// GET /key gets information and stats about a given API key
-	mux.Get(u.Conf.Prefix+"/key", http.HandlerFunc(a.SendHandler))
-
+	mux.Get(u.Conf.Prefix+"/key", http.HandlerFunc(a.UnimplementedHandler))
 	// POST /key generates a new API key with given information
-	mux.Post(u.Conf.Prefix+"/key", http.HandlerFunc(a.SendHandler))
+	mux.Post(u.Conf.Prefix+"/key", http.HandlerFunc(a.UnimplementedHandler))
+	// GET /nomsg gets information about a number's status on the do not message list
+	mux.Get(u.Conf.Prefix+"/nomsg", http.HandlerFunc(a.UnimplementedHandler))
+	// POST /nomsg adds a number to the do not message list
+	mux.Post(u.Conf.Prefix+"/nomsg", http.HandlerFunc(a.UnimplementedHandler))
+	// GET /number gets information and stats about a given number
+	mux.Get(u.Conf.Prefix+"/number", http.HandlerFunc(a.UnimplementedHandler))
+	//GET /host returns the container hostname
+	mux.Post(u.Conf.Prefix+"/host", http.HandlerFunc(a.HostHandler))
+	/*--- END HELPER ENDPOINTS ---*/
 
-	// Webserver Endpoints
+	/*--- START WEB ENDPOINTS ---*/
 	// GET / is the homepage (also the only page)
 	mux.Get("/", http.HandlerFunc(a.IndexHandler))
+	/*--- END WEB ENDPOINTS ---*/
 
 	// New Negroni Instance
 	n := negroni.New(
