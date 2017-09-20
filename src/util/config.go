dcec96cce98e8aaf19e8762154df3420d0e03214
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-19 23:13:22 -0400

    Added redis configuration and connection to config.go

--- src/util/config.go
+++ src/util/config.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 
+	redis "gopkg.in/redis.v5"
+
 	s "github.com/dechristopher/sms.party/src/strings"
 )
 
@@ -15,31 +17,64 @@ var (
 	Conf Configuration
 	// Templates stores statically compiled HTML templates
 	Templates = template.Must(template.ParseFiles("web/index.html"))
+	// R a redis client instance
+	R = redis.NewClient(&redis.Options{
+		Addr:     "",
+		Password: "",
+		DB:       1,
+	})
+	// RedisConnected is what it sounds like
+	RedisConnected = false
 )
 
 // Configuration for csms-backend
 type Configuration struct {
-	Port    string `json:"port"`
-	Prefix  string `json:"prefix"`
-	Rate    int    `json:"rate"`
-	Version string `json:"version"`
+	Port      string `json:"port"`
+	Prefix    string `json:"prefix"`
+	Rate      int    `json:"rate"`
+	Version   string `json:"version"`
+	RedisConf Redis  `json:"redisconf"`
+}
+
+// Redis configuration
+type Redis struct {
+	Address  string `json:"address"`
+	Password string `json:"password"`
+	DB       int    `json:"db"`
 }
 
 // ReadConfig reads config.json into memory for global configuration
 func ReadConfig() Configuration {
+	// Check to make sure config.json exists
 	raw, err := ioutil.ReadFile("./config.json")
 	if err != nil {
 		log.Fatal(s.LogPrefix + "Configuration Error! config.json improperly loaded or does not exist :: " + err.Error())
 		os.Exit(1)
 	}
 
+	// Unmarshal the raw json config into a Configuration object
 	var c Configuration
 	if errUnm := json.Unmarshal(raw, &c); errUnm != nil {
 		log.Fatal(s.LogPrefix + "Configuration Error! config.json improperly unmarshaled :: " + errUnm.Error())
 		os.Exit(1)
 	}
 
+	// Set the full API prefix slug
 	c.Prefix = c.Prefix + "/v" + c.Version
+
+	// Trash the fake init client and actually connect to the datastore
+	R = redis.NewClient(&redis.Options{
+		Addr:     c.RedisConf.Address,
+		Password: c.RedisConf.Password,
+		DB:       c.RedisConf.DB,
+	})
+
+	if rperr := R.Ping().Err(); rperr != nil {
+		LogErr(s.ErrRedisConnection)
+		RedisConnected = false
+	} else {
+		RedisConnected = true
+	}
 
 	return c
 }
