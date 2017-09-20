57c5ed7355fa6a8082181c7e04ef7ae0af11604c
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-19 23:31:13 -0400

    Commented and defined most handler function declarations in service.go

--- src/service.go
+++ src/service.go
@@ -36,15 +36,27 @@ func main() {
 	mux := bone.New()
 
 	// API Endpoints
-	// mux.Post(u.Conf.Prefix+"/batch", http.HandlerFunc(BatchHandler))
-	// mux.Post(u.Conf.Prefix+"/cast", http.HandlerFunc(CastHandler))
-	mux.Post(u.Conf.Prefix+"/send", http.HandlerFunc(a.SendHandler))
+	// POST /sms/batch sends a group of messages all sent to the API at once to conserve bandwidth
+	// mux.Post(u.Conf.Prefix+"/sms/batch", http.HandlerFunc(BatchHandler))
+
+	// POST /sms/cast sends a single SMS message to multiple numbers
+	// mux.Post(u.Conf.Prefix+"/sms/cast", http.HandlerFunc(CastHandler))
+
+	// POST /sms/send sends a single SMS message to a single number
+	mux.Post(u.Conf.Prefix+"/sms/send", http.HandlerFunc(a.SendHandler))
+
+	// GET /sms returns information about total app sms statistics
+	mux.Get(u.Conf.Prefix+"/sms", http.HandlerFunc(a.SendHandler))
 
 	// Helper Endpoints
+	// GET /key gets information and stats about a given API key
 	mux.Get(u.Conf.Prefix+"/key", http.HandlerFunc(a.SendHandler))
+
+	// POST /key generates a new API key with given information
 	mux.Post(u.Conf.Prefix+"/key", http.HandlerFunc(a.SendHandler))
 
 	// Webserver Endpoints
+	// GET / is the homepage (also the only page)
 	mux.Get("/", http.HandlerFunc(a.IndexHandler))
 
 	// New Negroni Instance
