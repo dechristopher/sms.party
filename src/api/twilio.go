ea0a5f5ca2189e8d9389c01b988f3804f6a70329
Andrew DeChristopher <andrewdechristopher@gmail.com>
2017-09-20 23:21:30 -0400

    Fixed up twilio.json. Still needs implementing

--- src/api/twilio.go
+++ src/api/twilio.go
@@ -1,10 +1,14 @@
 package api
 
+import (
+	u "github.com/dechristopher/sms.party/src/util"
+)
+
 // Credentials for twilio accounts
 type Credentials struct {
-	AccountSID string
-	AuthToken  string
-	FromNumber string
+	AccountSID string `json:"sid"`
+	AuthToken  string `json:"auth"`
+	FromNumber string `json:"num"`
 }
 
 // GetCreds returns a set pf Twilio credentials from Redis
@@ -12,6 +16,7 @@ func GetCreds() Credentials {
 	var creds Credentials
 
 	// Snag creds from redis here...
+	blob := u.GetCredsAsJSON()
 
 	creds.AccountSID = ""
 	creds.AuthToken = ""
